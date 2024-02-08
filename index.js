7dc2971854259163952eb7b10df9180c4cfa3efa
mlattard <maxime@lattard.fr>
2024-02-08 17:00:18 -0500

    update and delete working

--- index.js
+++ index.js
@@ -17,8 +17,29 @@ app.get("/add", (req, res) => {
     res.render("add.ejs");
 });
 
+app.get("/update/:id", (req, res) => {
+    const postId = req.params.id;
+
+    posts.forEach(post => {
+        if (postId == post.id) {
+            res.render("update.ejs", {postUpdate: post});
+        }
+    });
+});
+
+app.get("/delete/:id", (req, res) => {
+    const postId = req.params.id;
+
+    posts.forEach(post => {
+        if (postId == post.id) {
+            res.render("delete.ejs", {postDelete: post});
+        }
+    });
+});
+
 app.post("/submit", (req, res) => {
     let newPost = {
+        id: posts.length + 1, 
         title: req.body['title'],
         content: req.body['content'],
         date: new Date(),
@@ -28,6 +49,27 @@ app.post("/submit", (req, res) => {
     res.redirect("/");
 });
 
+app.post("/submit/:id", (req, res) => {
+    const postId = req.params.id;
+
+    posts.forEach(post => {
+        if (post.id == postId){
+            post.title = req.body['title'];
+            post.author = req.body['author'];
+            post.content = req.body['content'];
+            post.date = new Date();
+            } 
+        res.redirect("/");       
+    });
+});
+
+app.post("/delete/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+
+    posts = posts.filter(post => post.id !== postId);
+    res.redirect("/");       
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-  });
+});
